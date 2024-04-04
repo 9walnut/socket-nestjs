48c40a7b0e2816bcab56a4044fbcb4f3b2eb7c39
9walnut <kguho9202@gmail.com>
2024-04-04 14:03:27 +0900

    ✨ Add component
    add client component

--- be/src/chat/chat.gateway.ts
+++ be/src/chat/chat.gateway.ts
@@ -2,21 +2,40 @@ import {
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
-  WsResponse,
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
 } from '@nestjs/websockets';
-import { from, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { Server } from 'ws';
+import { Logger } from '@nestjs/common';
+import { Server, Socket } from 'socket.io';
+import { AddMessageDto } from './dto/add-message.dto';
 
-@WebSocketGateway(8080)
-export class ChatGateway {
+@WebSocketGateway({ cors: { origin: '*' } })
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server: Server;
 
-  @SubscribeMessage('message')
-  onEvent(client: any, data: any): Observable<WsResponse<number>> {
-    return from([1, 2, 3]).pipe(
-      map((item) => ({ event: 'events', data: item })),
-    );
+  private logger = new Logger('ChatGateway');
+
+  @SubscribeMessage('chat') // subscribe to chat event messages
+  handleMessage(@MessageBody() payload: AddMessageDto): AddMessageDto {
+    this.logger.log(`Message received: ${payload.author} - ${payload.body}`);
+    this.server.emit('chat', payload); // broadbast a message to all clients
+    return payload; // return the same payload data
   }
+
+  handleConnection(socket: Socket) {
+    this.logger.log(`Socket connected: ${socket.id}`);
+  }
+
+  handleDisconnect(socket: Socket) {
+    this.logger.log(`Socket disconnected: ${socket.id}`);
+  }
+
+  // @SubscribeMessage('message')
+  // onEvent(client: any, data: any): Observable<WsResponse<number>> {
+  //   return from([1, 2, 3]).pipe(
+  //     map((item) => ({ event: 'events', data: item })),
+  //   );
+  // }
 }
--- fe/src/App.js
+++ fe/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ChatClient from "./ChatClient"; // ChatClient 컴포넌트를 import
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Chat Application</p>
       </header>
+      <ChatClient /> {/* ChatClient 컴포넌트를 사용 */}
     </div>
   );
 }
