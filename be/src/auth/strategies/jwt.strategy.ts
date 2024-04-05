390bb39952ea6f18c2cb7304af053528d192730b
9walnut <kguho9202@gmail.com>
2024-04-05 14:26:40 +0900

    ✨ Add strategy
    add strategy

---
+++ be/src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,16 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly authService: AuthService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  async validate(payload: any) {
+    return { userId: payload.sub, username: payload.username };
+  }
+}
