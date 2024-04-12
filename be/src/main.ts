2ac19b4120f914b669f01f78cff0084a06b7c52d
9walnut <kguho9202@gmail.com>
2024-04-12 10:15:29 +0900

    ✨ Add Swagger
    add swagger base config

--- be/src/main.ts
+++ be/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { BaseAPIDocument } from './swagger.document';
+import { SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new BaseAPIDocument().initializeOptions();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(8080);
 }
 bootstrap();
