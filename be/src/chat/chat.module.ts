b22b75d983b0555df926ebee3a80d11dc7da26aa
9walnut <kguho9202@gmail.com>
2024-04-13 12:18:27 +0900

    ✨ Add chat imports
    add chatting module imports(ChatRoom, User)

--- be/src/chat/chat.module.ts
+++ be/src/chat/chat.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Chat } from './entities/chat.entity';
 import { ChatService } from './chat.service';
+import { ChatRoom } from './entities/chatroom.entity';
+import { User } from 'src/users/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Chat])],
+  imports: [TypeOrmModule.forFeature([Chat, ChatRoom, User])],
   providers: [ChatService],
   exports: [ChatService],
 })
