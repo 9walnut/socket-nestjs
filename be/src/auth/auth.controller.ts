640b589ca74648eed7d326970f0ea58201e2de82
9walnut <kguho9202@gmail.com>
2024-04-14 15:37:08 +0900

    ✨ Add swagger summary
    add swagger summary - signup

--- be/src/auth/auth.controller.ts
+++ be/src/auth/auth.controller.ts
@@ -2,12 +2,17 @@ import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from 'src/auth/dto/create-user.dto';
 import { LoginUserDto } from 'src/auth/dto/login-user.dto';
+import { ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signup')
+  @ApiBody({ type: CreateUserDto })
+  @ApiOperation({ summary: '회원가입' })
+  @ApiResponse({ status: 201, description: '회원가입 성공' })
+  @ApiResponse({ status: 403, description: 'Forbidden' })
   async signUp(@Body() createUserDto: CreateUserDto) {
     return this.authService.signUp(createUserDto);
   }
--- be/src/auth/dto/create-user.dto.ts
+++ be/src/auth/dto/create-user.dto.ts
@@ -1,12 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty({ description: '유저 아이디' })
   @IsString()
   userid: string;
 
+  @ApiProperty({ description: '유저 닉네임' })
   @IsString()
   username: string;
 
+  @ApiProperty({ description: '비밀번호' })
   @IsString()
   password: string;
 }
